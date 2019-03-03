af6b68382a9b2810a116084a82620c79854be975
Naitian Zhou <naitian@naitian.org>
2019-03-03 08:04:41 -0500

    Rescale Politeness

--- brain.py
+++ brain.py
@@ -7,10 +7,18 @@ def format_draft(draft, email_addr):
     return convokit.Utterance(id=1, user=email_addr, root=1, reply_to=None, text=draft)
 
 
+def rescale(politeness):
+    max_politeness = .8
+    pol = min(politeness['polite'], max_politeness)
+    pol = (pol + (1 - max_politeness) / 2) / ((max_politeness + 1) / 2)
+    impol = 1 - pol
+    return {'polite': pol, 'impolite': impol}
+
+
 def score_draft(draft, email_addr="ben@example.com"):
     utterance = format_draft(draft, email_addr)
     corpus = convokit.Corpus(utterances=[utterance])
     document = PolitenessFeatureVectorizer.preprocess([{'text': utterance.text}])[0]
-    politeness = model.score(document)
+    politeness = rescale(model.score(document))
     print(politeness)
     return politeness
